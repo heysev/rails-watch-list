556ff62bd4fbad84408afa3f855e9e1b15158da3
Severine Bader <severine.bader@gmail.com>
2023-11-17 09:04:40 +0100

    app v0

---
+++ app/models/bookmark.rb
@@ -0,0 +1,9 @@
+class Bookmark < ApplicationRecord
+  belongs_to :list
+  belongs_to :movie
+
+
+  validates :comment, presence: true, length: { minimum: 6 }
+  validates :movie, presence: true, :uniqueness => {:scope => :list_id}
+  validates :list, presence: true
+end
